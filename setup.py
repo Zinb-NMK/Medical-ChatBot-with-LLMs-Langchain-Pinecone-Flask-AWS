f450745801a1493d2ab63f5e5ff1f65a51e5b730
N.ManojKumar <nagarammanojkumar333@gmail.com>
2026-01-06 12:22:20 +0530

    Requirements are installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nagaram Manoj Kumar",
+    author_email="nagarammanojkumar107@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
